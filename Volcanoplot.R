5095c425d8c931113a09cae54a3947ef6f3da225
Muhammad Jamil <16117942+muhammadjamil@users.noreply.github.com>
2024-03-11 01:06:02 +0100

    Update Volcanoplot.R
    xlim argument removed and no more needed. Xlim will be decided based on absolute maximum value and volcano plot will have xlim from -absolute maximum to +absolute maximum

--- Volcanoplot.R
+++ Volcanoplot.R
@@ -1,10 +1,11 @@
 ### Function to create volcano plot
-volcanoPlot <- function(x,p.value=p.value,Difference=Difference,pval_threshod=0.05,sep="\t",dec=".",xlim=c(-5,5),
+volcanoPlot <- function(x,p.value=p.value,Difference=Difference,pval_threshod=0.05,sep="\t",dec=".",
                         output="volcanoplot.tiff",width=8,height=8,units="in",res=300){
   ## Reading file
   x = read.csv(x,sep=sep,header=TRUE,dec=dec)
   ## Creating tiff output
   tiff(filename = output,height=height,width=width,units=units,res=res)
+  xlim = c(-abs(max(x$Difference)),abs(max(x$Difference)))
   plot(x=x$Difference,y=-log10(x$p.value),pch=20,cex=1,col="Grey",xlim=xlim,xlab="Mean.Difference",ylab="-log10(p)")
   with(subset(x,p.value <= pval_threshod & abs(Difference) < 1),points(x=Difference,y=-log10(p.value),pch=20,cex=1.5,col="LightBlue"))
   with(subset(x,p.value <= pval_threshod & Difference <= -1),points(x=Difference,y=-log10(p.value),pch=20,cex=1.5,col="LightGreen"))
